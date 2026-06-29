74a2154e1918e0648fa95858a09292da652c22ab
nashitshahi17 <nashitshahi17@gmail.com>
2026-06-29 19:59:29 +0530

    new controllers added

---
+++ services/order-service/src/controllers/orderController.js
@@ -0,0 +1,49 @@
+const orderService = require("../services/orderService");
+const catchAsync = require("../utils/catchAsync");
+const { successResponse } = require("../utils/response");
+const HTTP_STATUS = require("../constants/httpStatus");
+const MESSAGES = require("../constants/messages");
+
+const handleCreateOrder = catchAsync(async (req, res) => {
+
+    const order = await orderService.createOrder(req.user.id,req.body);
+
+    return successResponse(res,HTTP_STATUS.CREATED,MESSAGES.ORDER.CREATE_SUCCESS,order);
+
+});
+
+
+const handleGetUserOrders = catchAsync(async (req, res) => {
+
+    const orders = await orderService.getUserOrders(req.user.id,req.query);
+
+    return successResponse(res,HTTP_STATUS.OK,MESSAGES.ORDER.FETCH_SUCCESS,orders);
+
+});
+
+const handleGetOrder = catchAsync(async (req, res) => {
+
+    const order = await orderService.getOrderById(req.params.id, req.user.id, req.user.role);
+
+    return successResponse(res,HTTP_STATUS.OK,MESSAGES.ORDER.FETCH_SUCCESS,order);
+
+});
+
+const handleUpdateOrderStatus = catchAsync(async (req, res) => {
+
+    const updatedOrder =await orderService.updateOrderStatus(req.params.id,req.body.status);
+    return successResponse(res,HTTP_STATUS.OK,MESSAGES.ORDER.UPDATE_SUCCESS,updatedOrder);
+});
+
+const handleDeleteOrder = catchAsync(async (req, res) => {
+    await orderService.deleteOrder(req.params.id);
+    return successResponse(res,HTTP_STATUS.OK,MESSAGES.ORDER.DELETE_SUCCESS);
+});
+
+module.exports = {
+    handleCreateOrder,
+    handleGetUserOrders,
+    handleGetOrder,
+    handleUpdateOrderStatus,
+    handleDeleteOrder
+};
