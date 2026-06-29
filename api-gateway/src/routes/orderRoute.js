7f49163d45bf5365588debb6dd9436196191ff55
nashitshahi17 <nashitshahi17@gmail.com>
2026-06-29 20:03:49 +0530

    order-route added

---
+++ api-gateway/src/routes/orderRoute.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const {createProxyMiddleware} = require('http-proxy-middleware')
+
+router.use(createProxyMiddleware({
+    target: process.env.ORDER_SERVICE_URL,
+    changeOrigin: true
+}))
+
+module.exports = router
