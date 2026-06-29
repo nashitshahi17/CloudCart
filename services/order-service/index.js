75faf88c1077c48b8c3a813b12628ec4d720ed63
nashitshahi17 <nashitshahi17@gmail.com>
2026-06-29 20:02:56 +0530

    order-service

--- services/order-service/index.js
+++ services/order-service/index.js
@@ -1,1 +1,22 @@
 require('dotenv').config()
+const express = require('express')
+const {connectDB} = require('./src/config/db')
+const orderRoutes = require('./src/routes/orderRoutes')
+
+const app = express()
+
+app.use(express.json())
+
+connectDB(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB Connected Successfully")
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+app.use("/", orderRoutes);
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on ${process.env.PORT}`)
+})
