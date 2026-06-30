95c67ce5c94480fe33b8de8903eeacf1fe285a7e
nashitshahi17 <nashitshahi17@gmail.com>
2026-06-30 18:42:08 +0530

    made pages

---
+++ frontend/pages/ErrorPage/Errorpage.jsx
@@ -0,0 +1,9 @@
+export default function ErrorPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-3xl font-bold">
+                Something went wrong!
+            </h1>
+        </div>
+    );
+}
---
+++ frontend/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import Loader from "../../components/common/Loader/Loader";
+
+export default function Login() {
+
+    return (
+
+        <div className="flex min-h-screen items-center justify-center">
+
+            <Loader />
+
+        </div>
+
+    );
+
+}
