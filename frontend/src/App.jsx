1cacf0d9b410c84b13fc69625d821834738b2fe4
nashitshahi17 <nashitshahi17@gmail.com>
2026-07-01 22:28:47 +0530

    modified app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "../router";
-import api from '../api/axios'
+import api from '../shared/api/axios'
 
 function App() {
     return <RouterProvider router={router} />;
