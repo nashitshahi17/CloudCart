cedaa0d2ba8c8691354149faa28ad2e4e23ae94b
nashitshahi17 <nashitshahi17@gmail.com>
2026-07-04 17:36:19 +0530

    added notification service

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -5,6 +5,7 @@ const userRoute = require('./src/routes/userRoute')
 const productRoute = require('./src/routes/productRoute')
 const orderRoute = require('./src/routes/orderRoute')
 const cartRoute = require('./src/routes/cartRoute')
+const notficationRoute = require('./src/routes/notificationRoute')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express()
@@ -17,5 +18,6 @@ app.use('/api/users',userRoute)
 app.use('/api/products',productRoute)
 app.use('/api/orders',orderRoute)
 app.use('/api/cart',cartRoute)
+app.use('/api/notifications',notficationRoute)
 
 app.listen(PORT,()=>{console.log(`Server Running on ${PORT}`)})
